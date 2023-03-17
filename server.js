b26be76c4bab8f9b4ab261ef872194e85544b53c
Konstantin Beloglazov <ajoq@ya.ru>
2023-03-17 20:52:24 +0300

    init

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({ json: true }));
+
+const notes = [
+    {
+        content:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, natus.',
+        id: 1,
+    },
+    {
+        content: 'This is some text within a card body',
+        id: 2,
+    },
+];
+let nextId = 3;
+
+const router = new Router();
+
+router.get('/notes', async (ctx, next) => {
+    ctx.response.body = notes;
+});
+
+router.post('/notes', async (ctx, next) => {
+    notes.push({ content: ctx.request.body, id: nextId++ });
+    ctx.response.status = 204;
+});
+
+router.delete('/notes/:id', async (ctx, next) => {
+    const noteId = Number(ctx.params.id);
+    const index = notes.findIndex((o) => o.id === noteId);
+    if (index !== -1) {
+        notes.splice(index, 1);
+    }
+    ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
